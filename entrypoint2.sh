8cf0736a3284553209518748fb126532df81c87a
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-06-27 16:32:31 +0500

    python version change

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -4,4 +4,4 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-gunicorn dockerdjango.wsgi:application --bind "80.89.228.71:8000"
+gunicorn dockerdjango.wsgi:application --bind "0.0.0.0:8000"
