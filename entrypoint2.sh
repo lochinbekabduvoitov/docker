cddb43ac45b9a2a6ea98d315c9b2e85dd8c5ef68
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-06-27 17:08:23 +0500

    add new feature

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -4,4 +4,4 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-gunicorn dockerdjango.wsgi:application --bind "80.89.228.71:9090"
+gunicorn dockerdjango.wsgi:application --bind "django_gunicorn:8000"
